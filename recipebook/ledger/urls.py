3f9714ac706a80623d096f81e8ffc4c79bb08089
fckjava <mattjoaquim37@gmail.com>
2025-02-24 08:48:21 -0800

    Templates for recipe 1 and 2 were constructed and fixed to be accessed in the Recipes list

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 # <appname>/urls.py
 from django.urls import path
-from .views import index
+from .views import recipes, recipe1, recipe2
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('recipes/list', recipes, name ='recipes'),
+    path('recipe/1', recipe1, name ='recipe1'),
+    path('recipe/2', recipe2, name ='recipe2'),
 ]
 
 app_name = "ledger"
