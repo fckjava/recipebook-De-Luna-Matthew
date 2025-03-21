cff1b0a78c5d6e58f762a324c671e27670a8ec9c
fckjava <mattjoaquim37@gmail.com>
2025-03-21 06:36:45 -0700

    Altered and added models, as well as necessary changes to add login/logout features

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import recipe_list, recipe_detail
 
 urlpatterns = [
-    path('recipes/list/', recipe_list, name ='recipes'),
+    path('recipes/list/', recipe_list, name ='recipe_list'),
     path('recipe/<int:recipe_id>/', recipe_detail, name='recipe_detail'),
 ]
 
